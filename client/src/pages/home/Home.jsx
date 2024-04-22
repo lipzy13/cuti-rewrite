de74f7e988a8fbfc5dae8b33e537152d1c279c63
alphaa <alifadha1@gmail.com>
2024-04-22 14:03:06 +0700

    Setting up the profile pages

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,18 +4,18 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import UserInfo from "../../components/userInfo/UserInfo";
 
 const Home = () => {
-	return (
-		<div className="flex w-screen min-h-screen">
-			<Sidebar />
-			<div className="bg-zinc-100 ms-60 w-full h-screen ">
-				<div className="grid grid-cols-4 mx-8 -mt-2 gap-4 p-4">
-					<UserInfo />
-					<BoxCuti />
-					<BoxKontrak />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex w-screen min-h-screen">
+      <Sidebar />
+      <div className="bg-zinc-100 ms-60 w-full h-screen ">
+        <div className="grid grid-cols-4 mx-8 -mt-2 gap-4 p-4">
+          <UserInfo />
+          <BoxCuti />
+          <BoxKontrak />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- client/src/pages/profil/Profil.jsx
+++ client/src/pages/profil/Profil.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Profil = () => {
-	return (
-		<div>
-			<Sidebar />
-			Profil
-		</div>
-	);
+  return (
+    <div className="flex w-screen min-h-screen">
+      <Sidebar />
+      <div className="bg-zinc-100 ms-60 w-full h-screen ">
+        <div className="bg-white p-4 rounded-md  shadow-xl m-4">Profil</div>
+      </div>
+    </div>
+  );
 };
 
 export default Profil;
