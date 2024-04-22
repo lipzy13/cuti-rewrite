0db6c1838c3785be5d22c0beeee0f9cf38aafb89
alphaa <alifadha1@gmail.com>
2024-04-22 22:17:28 +0700

    adding profile page

--- client/src/pages/kontrak/Kontrak.jsx
+++ client/src/pages/kontrak/Kontrak.jsx
@@ -29,80 +29,21 @@ const Kontrak = () => {
     <div className="flex w-screen min-h-screen">
       <Sidebar />
       <div className="bg-zinc-100 ms-60 w-full h-screen ">
-        <div className="bg-white p-4 rounded-md shadow-xl my-4 mx-8">
-          <h1 className="font-bold text-center ">Kontrak</h1>
-          <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="bg-white p-4 rounded-md shadow-xl m-8">
+          <h1 className="font-bold text-center text-2xl m-2">Kontrak</h1>
+          <div className="max-w-[85rem] p-8 sm:px-6 lg:px-8 lg:py-14 mx-auto">
             <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
               <div className="flex flex-col bg-white border shadow-sm rounded-xl">
                 <div className="p-4 md:p-5">
                   <div className="flex items-center gap-x-2">
                     <p className="text-xs uppercase tracking-wide text-gray-500">
-                      Total users
-                    </p>
-                    <div className="hs-tooltip">
-                      <div className="hs-tooltip-toggle">
-                        <svg
-                          className="flex-shrink-0 size-4 text-gray-500"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <circle cx="12" cy="12" r="10" />
-                          <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-                          <path d="M12 17h.01" />
-                        </svg>
-                        <span
-                          className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded shadow-sm"
-                          role="tooltip"
-                        >
-                          The number of daily users
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="mt-1 flex items-center gap-x-2">
-                    <h3 className="text-xl sm:text-2xl font-medium text-gray-800">
-                      72,540
-                    </h3>
-                    <span className="flex items-center gap-x-1 text-green-600">
-                      <svg
-                        className="inline-block size-4 self-center"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <polyline points="22 7 13.5 15.5 8.5 10.5 2 17" />
-                        <polyline points="16 7 22 7 22 13" />
-                      </svg>
-                      <span className="inline-block text-sm">1.7%</span>
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col bg-white border shadow-sm rounded-xl">
-                <div className="p-4 md:p-5">
-                  <div className="flex items-center gap-x-2">
-                    <p className="text-xs uppercase tracking-wide text-gray-500">
-                      Sessions
+                      Nomor Kontrak
                     </p>
                   </div>
 
                   <div className="mt-1 flex items-center gap-x-2">
                     <h3 className="text-xl sm:text-2xl font-medium text-gray-800">
-                      29.4%
+                      {kontrak.nomorKontrak}
                     </h3>
                   </div>
                 </div>
@@ -111,32 +52,21 @@ const Kontrak = () => {
                 <div className="p-4 md:p-5">
                   <div className="flex items-center gap-x-2">
                     <p className="text-xs uppercase tracking-wide text-gray-500">
-                      Avg. Click Rate
+                      Tanggal Mulai
                     </p>
                   </div>
 
                   <div className="mt-1 flex items-center gap-x-2">
                     <h3 className="text-xl sm:text-2xl font-medium text-gray-800">
-                      56.8%
+                      {new Date(kontrak.tanggalMulai).toLocaleDateString(
+                        "id-Id",
+                        {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        },
+                      )}
                     </h3>
-                    <span className="flex items-center gap-x-1 text-red-600">
-                      <svg
-                        className="inline-block size-4 self-center"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <polyline points="22 17 13.5 8.5 8.5 13.5 2 7" />
-                        <polyline points="16 17 22 17 22 11" />
-                      </svg>
-                      <span className="inline-block text-sm">1.7%</span>
-                    </span>
                   </div>
                 </div>
               </div>
@@ -144,13 +74,35 @@ const Kontrak = () => {
                 <div className="p-4 md:p-5">
                   <div className="flex items-center gap-x-2">
                     <p className="text-xs uppercase tracking-wide text-gray-500">
-                      Pageviews
+                      Tanggal Selesai
                     </p>
                   </div>
 
                   <div className="mt-1 flex items-center gap-x-2">
                     <h3 className="text-xl sm:text-2xl font-medium text-gray-800">
-                      92,913
+                      {new Date(kontrak.tanggalSelesai).toLocaleDateString(
+                        "id-Id",
+                        {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        },
+                      )}
+                    </h3>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col bg-white border shadow-sm rounded-xl">
+                <div className="p-4 md:p-5">
+                  <div className="flex items-center gap-x-2">
+                    <p className="text-xs uppercase tracking-wide text-gray-500">
+                      Sisa Cuti
+                    </p>
+                  </div>
+
+                  <div className="mt-1 flex items-center gap-x-2">
+                    <h3 className="text-xl sm:text-2xl font-medium text-gray-800">
+                      {kontrak.sisaCuti}
                     </h3>
                   </div>
                 </div>
--- client/src/pages/profil/Profil.jsx
+++ client/src/pages/profil/Profil.jsx
@@ -1,11 +1,144 @@
+import { useContext } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { AuthContext } from "../../context/authContext";
 
 const Profil = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="flex w-screen min-h-screen">
       <Sidebar />
       <div className="bg-zinc-100 ms-60 w-full h-screen ">
-        <div className="bg-white p-4 rounded-md  shadow-xl m-4">Profil</div>
+        <div className="bg-white p-4 rounded-md  shadow-xl m-4">
+          <div className="text-center my-8">
+            <h1 className="font-bold text-2xl">Profil</h1>
+          </div>
+          <form>
+            <div className="grid sm:grid-cols-12 gap-2 sm:gap-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="nama"
+                  className="inline-block text-sm text-gray-800 mt-2.5 dark:text-gray-200"
+                >
+                  Nama
+                </label>
+              </div>
+              <div className="sm:col-span-9">
+                <div className="sm:flex">
+                  <input
+                    id="nama"
+                    type="text"
+                    disabled
+                    className="py-2 border px-3 pe-11 block w-full border-gray-200 shadow-sm -mt-px -ms-px rounded-lg sm:mt-0 sm:first:ms-0 text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-100 disabled:pointer-events-none"
+                    placeholder={user.nama}
+                  ></input>
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="nik"
+                  className="inline-block text-sm text-gray-800 mt-2.5 dark:text-gray-200"
+                >
+                  NIK
+                </label>
+              </div>
+
+              <div className="sm:col-span-9">
+                <div className="sm:flex">
+                  <input
+                    id="nik"
+                    type="text"
+                    disabled
+                    className="py-2 border px-3 pe-11 block w-full border-gray-200 shadow-sm -mt-px -ms-px rounded-lg sm:mt-0 sm:first:ms-0 text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-100 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-399 dark:focus:ring-gray-600"
+                    placeholder={user.nik}
+                  ></input>
+                </div>
+              </div>
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="jabatan"
+                  className="inline-block text-sm text-gray-800 mt-2.5 dark:text-gray-200"
+                >
+                  Jabatan
+                </label>
+              </div>
+              <div className="sm:col-span-9">
+                <div className="sm:flex">
+                  <input
+                    id="jabatan"
+                    disabled
+                    type="text"
+                    className="py-2 border px-3 pe-11 block w-full border-gray-200 shadow-sm -mt-px -ms-px rounded-lg sm:mt-0 sm:first:ms-0 text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-100 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                    placeholder={user.jabatan}
+                  ></input>
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="tanggalMasuk"
+                  className="inline-block text-sm text-gray-800 mt-2.5 dark:text-gray-200"
+                >
+                  Tanggal Masuk
+                </label>
+              </div>
+              <div className="sm:col-span-9">
+                <div className="sm:flex">
+                  <input
+                    id="tanggalMasuk"
+                    disabled
+                    type="text"
+                    className="py-2 border px-3 pe-11 block w-full border-gray-200 shadow-sm -mt-px -ms-px rounded-lg sm:mt-0 sm:first:ms-0 text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-100 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                    placeholder={new Date(user.tanggalMasuk).toLocaleDateString(
+                      "id-Id",
+                      {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      },
+                    )}
+                  ></input>
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label className="inline-block text-sm text-gray-800 mt-2.5 dark:text-gray-200">
+                  Password
+                </label>
+              </div>
+
+              <div className="sm:col-span-9">
+                <div className="space-y-2">
+                  <input
+                    id="passwordLama"
+                    name="passwordLama"
+                    type="password"
+                    className="py-2 border px-3 pe-11 block w-full {{$errors->has('old_password') ? 'border-red-500' : 'border-gray-200' }}  shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                    placeholder="Masukkan password lama"
+                    aria-describedby="passwordLama"
+                  ></input>
+                  <input
+                    id="passwordBaru"
+                    name="passwordBaru"
+                    type="password"
+                    className="py-2 border px-3 pe-11 block w-full {{$errors->has('old_password') ? 'border-red-500' : 'border-gray-200' }}  shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                    placeholder="Masukkan password baru"
+                    aria-describedby="passwordBaru"
+                  ></input>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-5 flex justify-end gap-x-2">
+              <button
+                type="submit"
+                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
