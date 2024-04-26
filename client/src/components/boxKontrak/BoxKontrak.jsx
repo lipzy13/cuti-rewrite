40ee407dbabce250c2cf49639d8addac976238ad
alphaa <alifadha1@gmail.com>
2024-04-26 08:33:44 +0700

    Remove dark mode classes

--- client/src/components/boxKontrak/BoxKontrak.jsx
+++ client/src/components/boxKontrak/BoxKontrak.jsx
@@ -4,79 +4,79 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
 
 const BoxKontrak = ({ kontrak }) => {
-  const { user } = useContext(AuthContext);
-  return (
-    <div className="bg-white p-4 rounded-md col-start-1 col-end-5 shadow-xl my-4">
-      <div className="items-center justify-center w-full">
-        <div className="hs-accordion-group">
-          <div className="flex flex-col">
-            <div className="-m-1.5 overflow-x-auto">
-              <div className="p-1.5 min-w-full inline-block align-middle">
-                <div className="overflow-hidden border rounded-lg shadow">
-                  <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                    <thead className="bg-gray-50">
-                      <tr>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                        >
-                          No
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                        >
-                          Nomor Kontrak
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                        ></th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
-                        ></th>
-                      </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                      {kontrak.length > 0 ? (
-                        kontrak.map((k, index) => (
-                          <tr key={k._id}>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                              {index + 1}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                              {k.nomorKontrak}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"></td>
-                            <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                              <Link
-                                to={`/${user._id}/${k._id}`}
-                                className="text-xl font-bold text-blue-600"
-                              >
-                                <MdInfoOutline />
-                              </Link>
-                            </td>
-                          </tr>
-                        ))
-                      ) : (
-                        <tr>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"></td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                            Kontrak tidak ada
-                          </td>
-                        </tr>
-                      )}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const { user } = useContext(AuthContext);
+	return (
+		<div className="bg-white p-4 rounded-md col-start-1 col-end-5 shadow-xl my-4">
+			<div className="items-center justify-center w-full">
+				<div className="hs-accordion-group">
+					<div className="flex flex-col">
+						<div className="-m-1.5 overflow-x-auto">
+							<div className="p-1.5 min-w-full inline-block align-middle">
+								<div className="overflow-hidden border rounded-lg shadow">
+									<table className="min-w-full divide-y divide-gray-200 ">
+										<thead className="bg-gray-50">
+											<tr>
+												<th
+													scope="col"
+													className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+												>
+													No
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+												>
+													Nomor Kontrak
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+												></th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+												></th>
+											</tr>
+										</thead>
+										<tbody className="divide-y divide-gray-200 ">
+											{kontrak.length > 0 ? (
+												kontrak.map((k, index) => (
+													<tr key={k._id}>
+														<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+															{index + 1}
+														</td>
+														<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 ">
+															{k.nomorKontrak}
+														</td>
+														<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 "></td>
+														<td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+															<Link
+																to={`/${user._id}/${k._id}`}
+																className="text-xl font-bold text-blue-600"
+															>
+																<MdInfoOutline />
+															</Link>
+														</td>
+													</tr>
+												))
+											) : (
+												<tr>
+													<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 "></td>
+													<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 ">
+														Kontrak tidak ada
+													</td>
+												</tr>
+											)}
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default BoxKontrak;
--- client/src/components/tabelCuti/TabelCuti.jsx
+++ client/src/components/tabelCuti/TabelCuti.jsx
@@ -1,77 +1,77 @@
 const TabelCuti = ({ cuti }) => {
-  return (
-    <div className="bg-white p-4 rounded-md col-start-1 col-end-5 shadow-xl my-4">
-      <div className="items-center justify-center w-full">
-        <div className="hs-accordion-group">
-          <div className="flex flex-col">
-            <div className="-m-1.5 overflow-x-auto">
-              <div className="p-1.5 min-w-full inline-block align-middle">
-                <div className="overflow-hidden border rounded-lg shadow">
-                  <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                    <thead className="bg-gray-50">
-                      <tr>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                        >
-                          No
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                        >
-                          Tanggal Cuti
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                        >
-                          Jenis Cuti
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
-                        ></th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
-                        ></th>
-                      </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                      {cuti.map((cut, index) => (
-                        <tr key={cut._id}>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                            {index + 1}
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                            <ul>
-                              {cut.tanggal.map((el) => (
-                                <li key={el}>
-                                  {new Date(el).toLocaleDateString("id-Id", {
-                                    year: "numeric",
-                                    month: "long",
-                                    day: "numeric",
-                                  })}
-                                </li>
-                              ))}
-                            </ul>
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                            {cut.jenis}
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-white p-4 rounded-md col-start-1 col-end-5 shadow-xl my-4">
+			<div className="items-center justify-center w-full">
+				<div className="hs-accordion-group">
+					<div className="flex flex-col">
+						<div className="-m-1.5 overflow-x-auto">
+							<div className="p-1.5 min-w-full inline-block align-middle">
+								<div className="overflow-hidden border rounded-lg shadow">
+									<table className="min-w-full divide-y divide-gray-200 ">
+										<thead className="bg-gray-50">
+											<tr>
+												<th
+													scope="col"
+													className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+												>
+													No
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+												>
+													Tanggal Cuti
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+												>
+													Jenis Cuti
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+												></th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+												></th>
+											</tr>
+										</thead>
+										<tbody className="divide-y divide-gray-200 ">
+											{cuti.map((cut, index) => (
+												<tr key={cut._id}>
+													<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 ">
+														{index + 1}
+													</td>
+													<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 ">
+														<ul>
+															{cut.tanggal.map((el) => (
+																<li key={el}>
+																	{new Date(el).toLocaleDateString("id-Id", {
+																		year: "numeric",
+																		month: "long",
+																		day: "numeric",
+																	})}
+																</li>
+															))}
+														</ul>
+													</td>
+													<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 ">
+														{cut.jenis}
+													</td>
+												</tr>
+											))}
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default TabelCuti;
